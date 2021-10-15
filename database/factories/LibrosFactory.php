307bedcf5d866f2a79ffa8754456fae29a4e26ca
salatielatenco <carlossalatielatenco@gmail.com>
2021-10-15 13:35:06 -0500

    Initial commit

---
+++ database/factories/LibrosFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Libros;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LibrosFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Libros::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'titulo' => $this->faker->name(),
+            'autor' => $this->faker->name(),
+            'editorial' => $this->faker->name(),
+            'fecha_de_publicacion'=> $this->faker->date(),
+            'numero_de_paginas' => $this->faker->date()>
+
+        ];
+    }
+}
